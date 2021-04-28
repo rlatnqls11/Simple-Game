e92372d8ddd0fb24d86f99bcef6f872cd105cde5
rlatnqls11 <subin0940@gmail.com>
2021-04-28 21:41:17 +0900

    Really Really Final Version of simpleGame.py

--- simpleGame.py
+++ simpleGame.py
@@ -4,7 +4,7 @@ import tkinter as tk
 
 from tkinter import messagebox
  
-ans = [
+quest = [
 		#1
 		("① 대구대학교는 몇년도에 개교했는가?","1956년"),
 		#2
@@ -15,45 +15,45 @@ ans = [
                 ("④ 대구대학교의 교화는 무엇인가?","백일홍"),
                 #5
                 ("⑤ 다음 중 대구대학교로 오는 버스가 아닌것은?(1,2,3,4 중 택1)\n   1. 708\n   2. 818\n   3. 814\n   4. 651 ","4"),
-                #6
+                #1
 		("⑥ 대구대학교의 설립자는 누구인가?","이영식 목사"),
-		#7
+		#2
 		("⑦ 대구대학교의 건학이념은 무엇인가?","사랑 빛 자유"),
-		#8
+		#3
 		("⑧ 다음 중 대구대학교에 존재하는 장소가 아닌것은?(1,2,3,4 중 택1)\n   1. 두류공원\n   2. 글로벌라운지\n   3. 점자도서관\n   4. 창파도서관 ","1"),
-                #9
+                #4
                 ("⑨ 대구대학교 안에서 즐길 수 있는 스포츠로 옳지 않은것은?(1,2,3,4 중 택1)\n   1. 탁구\n   2. 스키\n   3. 골프\n   4. 수영","2"),
-                #10
+                #5
                 ("⑩ 대구대학교의 교훈은 무엇인가?","큰 뜻을 품어라")
  
 ]
 
-numdom = len(ans)
+numdom = len(quest)
 score = 0
-totalQuestions = 0
+num = 0
 
 def d1():
-	global totalQuestions, score, entry
-	if totalQuestions == numdom:
+	global num, score, entry
+	if num == numdom:
 	    text.pack_forget()
 	    entry.pack_forget()
-	    percent = (score/totalQuestions) * 100
+	    percent = score / num * 100
 		
 	    button['text'] = f"평균: {percent}%\n 점수:{score}점\n 합격여부를 확인할려면 클릭하세요"
 	    button['command'] = game_over
 	    button.pack()
 	    return
 	    
-	if totalQuestions == 0:
+	if num == 0:
 	    answer_widget()
 		
 	text['height'] = 7
 	text['bg'] = 'white'
 	text['width'] = 80
 	text.delete("1.0",tk.END)
-	text.insert("1.0",ans[totalQuestions][0])
+	text.insert("1.0",quest[num][0])
 	button.pack_forget()
-	totalQuestions+=1
+	num+=1
 
 def game_over():
 	global score
@@ -71,14 +71,14 @@ def answer_widget():
 	entry.focus()
  
 def empty_textbox():
-	ans.set("")
-	d1()
+		ans.set("")
+		d1()
  
 def check():
 	global n, score
 	text.delete("1.0",tk.END)
 	
-	if ans.get() == [totalQuestions-1][1]:
+	if ans.get() == quest[num-1][1]:
 		text.insert(tk.END, "정답입니다! :) ")
 		score+=1
 		text['bg'] = "green"
@@ -126,17 +126,8 @@ button = tk.Button(window, text="게임시작", bg="black", fg="white", command=
 button.pack()
 
 image_button.place(x=780, y=300)
+
 ans = tk.StringVar()
+
 window.mainloop()
 
-
-
-
-
-
-
-
-
-
-
-    
